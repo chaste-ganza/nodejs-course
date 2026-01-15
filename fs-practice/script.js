1c8122a78ac3b89a69da48310e546f81d7ca934e
chaste-ganza <gchaste23@gmail.com>
2026-01-15 19:47:47 +0200

    added some comments to remember next time

--- fs-practice/script.js
+++ fs-practice/script.js
@@ -1,20 +1,22 @@
 const fs = require("fs")
-
+//This is the function that contains info to be pushed in the json file
 function addChaste(list){
     list.name.push("Chaste")
     list.gender.push("male")
     list.specilialization.push("AI-engineer")
 }
 
-fs.readFile("studentList.json","utf-8",(err,data)=>{
+//This is the line that read the content of the file
+fs.readFile("studentList.json","utf-8",(err,data)=>{  
     if(err){console.log("Error occured while reading the file")}
     console.log("\n\nThe list we received from JSON file: ",data)
+    //data come as string, so we need to turn it into an object
     let receivedList = JSON.parse(data)
     console.log("\n\nAs you can see, there is nothing in this array: ",receivedList.name)
     addChaste(receivedList)
     console.log("\n\nThe list after adding values: ",receivedList)
 
-    //Let me declare a variable that stores the converted list to send to JSON file
+    //I declared a variable that stores the converted list to send to JSON file
     let finalList = JSON.stringify(receivedList)
 
     fs.writeFile("studentList.json",finalList,(err)=>{
